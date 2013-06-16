28a669491647f8abecc298586f6b8099614682bd
Erik Chancy <teh-boss@thechancy.com>
2013-06-16 13:42:54 +0200

    Added a tester
    Use to test code that is for quizes or is not to be saved for later.

---
+++ tester.py
@@ -0,0 +1,3 @@
+given_string2 = "I'm %(nickname)s. My real name is %(name)s, but my friends call me %(nickname)s."
+def sub_m(name, nickname):
+    return given_string2 %{"nickname": nickname, "name": name}
